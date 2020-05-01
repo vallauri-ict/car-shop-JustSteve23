492462290c0f9902396f71e472522373e131ee4c
JustSteve23 <61805356+JustSteve23@users.noreply.github.com>
2020-05-01 16:41:07 +0200

    Add files via upload

--- venditaVeicoliDLLProject/DB.cs
+++ venditaVeicoliDLLProject/DB.cs
@@ -7,7 +7,7 @@ namespace venditaVeicoliDLLProject
 {
     public class DB
     {
-        public static void createTableCars(string connectionStr)
+        public static string createTableVehicles(string connectionStr)
         {
             if (connectionStr != null)
             {
@@ -21,31 +21,27 @@ namespace venditaVeicoliDLLProject
                     try
                     {
                         command.CommandText = @"CREATE TABLE veicoli(
-                                            id int identity(1,1) PRIMARY KEY,type VARCHAR(10),
+                                            id INT identity(1,1) PRIMARY KEY,type VARCHAR(10),
                                             marca VARCHAR(255),modello VARCHAR(255),cilindrata INT,
                                             potenzaKw FLOAT,dataImmatricolazione DATE,kmPercorsi INT,
-                                            colore VARCHAR(25),isUsato BIT,isKm0 BIT,prezzo INT);";
+                                            colore VARCHAR(25),isUsato VARCHAR(2),isKm0 VARCHAR(2),info VARCHAR(255),prezzo INT);";
                         command.ExecuteNonQuery();
+                        command.CommandText = "INSERT INTO veicoli(type,marca,modello,cilindrata,potenzaKw,dataimmatricolazione,kmPercorsi,colore,isUsato,isKm0,info,prezzo) VALUES('auto','Ferrari','488Pista',4000,589.24,#10/08/2019#,0,'Rosso','NO','NO','numero airbag 8',400000);";
+                        command.ExecuteNonQuery();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        return "\nTabella auto creata!";
                     }
                     catch (OleDbException ex)
                     {
-                        Console.WriteLine("\n\n" + ex.Message);
-                        System.Threading.Thread.Sleep(2000);
-                        return;
+                        return "\n" + ex.Message;
                     }
-
-                    command.CommandText = "INSERT INTO veicoli(id,type,marca,modello,cilindrata,potenzaKw,dataimmatricolazione,kmPercorsi,colore,isUsato,isKm0,prezzo) VALUES(1,'auto','Ferrari','488Pista',4000,589.24,10/08/2019,0,'red',0,0,400000);";
-                    command.ExecuteNonQuery();
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("\n\nTabella auto creata!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    System.Threading.Thread.Sleep(2000);
                 }
             }
+            return "";
         }
 
-        private static void ListCars(string connectionStr)
+        public static void visualizeTableConsole(string connectionStr)
         {
             if (connectionStr != null)
             {
@@ -53,21 +49,112 @@ namespace venditaVeicoliDLLProject
                 using (connection)
                 {
                     connection.Open();
-                    OleDbCommand command = new OleDbCommand("SELECT * FROM cars", connection);
+                    OleDbCommand command = new OleDbCommand("SELECT * FROM veicoli", connection);
                     OleDbDataReader reader = command.ExecuteReader();
 
                     if (reader.HasRows)
                     {
                         Console.WriteLine("\n");
+                        Console.WriteLine("---------VEICOLI---------\n");
                         while (reader.Read())
-                            Console.WriteLine($"ID{reader.GetInt32(0)}\n");
+                            Console.WriteLine($"ID: {reader.GetInt32(0)}\n" +
+                                $"Type: {reader.GetString(1)}\n" +
+                                $"Marca: {reader.GetString(2)}\n" +
+                                $"Modello: {reader.GetString(3)}\n" +
+                                $"Cilindrata: {reader.GetInt32(4)}\n" +
+                                $"Potenza(KW): {reader.GetDouble(5)}\n" +
+                                $"Data Immatricolazione: {reader.GetDateTime(6).ToShortDateString()}\n" +
+                                $"Chilometri percorsi: {reader.GetInt32(7)}Km\n" +
+                                $"Colore: {reader.GetString(8)}\n" +
+                                $"Usato: {reader.GetString(9)}\n" +
+                                $"Km0: {reader.GetString(10)}\n"+
+                                $"Informazioni: {reader.GetString(11)}\n" +
+                                $"Prezzo: {reader.GetInt32(12)}$\n"+
+                                "\n-------------------------\n");
                     }
                     else
-                        Console.WriteLine("\n\nRiga non trovata");
+                        Console.WriteLine("\nTabella vuota");
                     reader.Close();
                 }
-                Console.WriteLine("\nVeicolo aggiunto");
-                System.Threading.Thread.Sleep(5000);
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+
+        public static string addVehicle(string connectionStr, string type, string marca, string modello, int cilindrata, double kw, DateTime dImm, int kmPercorsi, string colore, string usato, string km0, string info, int prezzo)
+        {
+            if (connectionStr != null)
+            {
+                OleDbConnection connection = new OleDbConnection(connectionStr);
+                using (connection)
+                {
+                    connection.Open();
+                    OleDbCommand command = new OleDbCommand();
+                    command.Connection = connection;
+
+                    string SQLquery = "INSERT INTO veicoli(type,marca,modello,cilindrata,potenzaKw,dataimmatricolazione,kmPercorsi,colore,isUsato,isKm0,info,prezzo) VALUES(@type,@marca,@modello,@cilindrata,@potenzaKw,@dataimmatricolazione,@kmPercorsi,@colore,@isUsato,@isKm0,@info,@prezzo)";
+                    command.CommandText = SQLquery;
+
+                    command.Parameters.Add(new OleDbParameter("@type", OleDbType.VarChar, 10)).Value = type;
+                    command.Parameters.Add("@marca", OleDbType.VarChar, 255).Value = marca;
+                    command.Parameters.Add("@modello", OleDbType.VarChar, 255).Value = modello;
+                    command.Parameters.Add("@cilindrata", OleDbType.Integer).Value = cilindrata;
+                    command.Parameters.Add("@potenzaKw", OleDbType.Double).Value = kw;
+                    command.Parameters.Add("@dataimmatricolazione", OleDbType.Date).Value = dImm.ToShortDateString();
+                    command.Parameters.Add("@kmPercorsi", OleDbType.Integer).Value = kmPercorsi;
+                    command.Parameters.Add("@colore", OleDbType.VarChar, 25).Value = colore;
+                    command.Parameters.Add("@isKm0", OleDbType.VarChar, 2).Value = km0;
+                    command.Parameters.Add("@isUsato", OleDbType.VarChar, 2).Value = usato;
+                    command.Parameters.Add("@info", OleDbType.VarChar, 255).Value = info;
+                    command.Parameters.Add("@prezzo", OleDbType.Integer).Value = prezzo;
+
+                    command.Prepare();
+                    command.ExecuteNonQuery();
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return "\nVeicolo inserito correttamente!!";
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                return "\n!!ERROR!!";
+            }
+        }
+
+        public static string deleteVehicle(string connectionStr,int id)
+        {
+            if (connectionStr != null)
+            {
+                OleDbConnection connection = new OleDbConnection(connectionStr);
+                using (connection)
+                {
+                    try
+                    {
+                        connection.Open();
+                        OleDbCommand command = new OleDbCommand();
+                        command.Connection = connection;
+
+                        string SQLquery = "DELETE FROM veicoli WHERE id=@id;";
+                        command.CommandText = SQLquery;
+                        var parameter = new OleDbParameter("@id", OleDbType.Integer);
+                        parameter.Value = id;
+                        command.Parameters.Add(parameter);
+                        command.Prepare();
+                        command.ExecuteNonQuery();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        return "";
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        return "\n"+ex.Message;
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                return "\n!!ERROR!!";
             }
         }
     }
